1cea265d008ffe3c375cf1c1776cc3e5312f76b9
Oprea Andre-Dorin <andrei_dorin.oprea@stud.acs.upb.ro>
2022-04-07 16:32:04 +0300

    A2

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.java
@@ -3,11 +3,11 @@ package ro.pub.cs.systems.eim.practicaltest01var04;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class PracticalTest01Var04MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_practical_test01_var04_main);
     }
 }
